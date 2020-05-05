a63c49826754f839fc5082f19a533a58555b3a85
santosh mahto <mahtos480@gmail.com>
2020-05-05 10:18:54 +0530

    session code

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.get('/ping', (req, res) => {
 
 // create contact doc
 app.post('/contact', (req, res) => {
+  console.log(req.body);
+
   res.status(200).send(
     JSON.stringify({
       status: true,
